68adfe76907b5f32303254682caf0f9ee8fd9007
manickcodebuddy <manick@codebuddy.co>
2024-12-28 15:42:18 +0530

    added heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>This is Code review tester app</h1>
+    </div>
+  );
 };
 
 export default HomePage;
